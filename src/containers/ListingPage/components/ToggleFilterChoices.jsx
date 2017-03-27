02b9742a7bf1cb9749d25e13a2aa65fe6b086b79
Tanja Gruschke <tmg@knowit.no>
2017-03-27 16:47:57 +0200

    #255 remove unused import

--- src/containers/ListingPage/components/ToggleFilterChoices.jsx
+++ src/containers/ListingPage/components/ToggleFilterChoices.jsx
@@ -5,15 +5,6 @@
  *  LICENSE file in the root directory of this source tree.
  *
  */
-
-import {
-    addEventListenerForResize,
-    updateIFrameDimensions,
-    addAsideClickListener,
-    removeEventListenerForResize,
-    removeAsideClickListener,
-} from 'ndla-article-scripts';
-
 import React, { PropTypes, Component } from 'react';
 import { ListingShape } from '../../../shapes';
 import FilterChoices from './FilterChoices';
