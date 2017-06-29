d60a9a2274869609fb4c75aabb2b223090500457
Tanja Gruschke <tmg@knowit.no>
2017-06-29 15:28:16 +0200

    #442 use helmet as security header

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import helmet from 'helmet';
 import { renderToString } from 'react-dom/server';
 import express from 'express';
 import compression from 'compression';
@@ -36,12 +37,107 @@ app.use(compression());
 app.use(express.static('htdocs', {
   maxAge: 1000 * 60 * 60 * 24 * 365, // One year
 }));
-app.use((req, res, next) => {
-  // TODO use helmet for security headers in stead
-  // res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-  // res.setHeader('Content-Security-Policy', 'default-src \'self\'; script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://*.ndla.no https://players.brightcove.net https://www.nrk.no https://www.googletagmanager.com https://www.google-analytics.com https://www.youtube.com https://s.ytimg.com https://cdn.auth0.com; style-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://fonts.googleapis.com https://fonts.gstatic.com; font-src \'self\' https://fonts.googleapis.com https://fonts.gstatic.com; connect-src \'self\' https://*.ndla.no https://logs-01.loggly.com; img-src https://*.ndla.no https://www.google-analytics.com https://stats.g.doubleclick.net data: ;');
-  next();
-});
+
+app.use(
+  helmet({
+    hsts: {
+      maxAge: 31536000,
+      includeSubDomains: true,
+    },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'", 'blob:'],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          " 'unsafe-eval'",
+          'https://*.ndla.no',
+          'https://players.brightcove.net',
+          'http://players.brightcove.net',
+          'https://players.brightcove.net',
+          '*.nrk.no',
+          'https://www.googletagmanager.com',
+          'https://www.google-analytics.com',
+          'https://www.youtube.com',
+          'https://s.ytimg.com',
+          'https://cdn.auth0.com',
+          'https://vjs.zencdn.net',
+          'https://httpsak-a.akamaihd.net',
+          '*.brightcove.com',
+          '*.brightcove.net',
+          'bcove.me',
+          'bcove.video',
+          '*.api.brightcove.com',
+          '*.o.brightcove.com',
+          'players.brightcove.net',
+          'hls.ak.o.brightcove.com',
+          'uds.ak.o.brightcove.com',
+          'brightcove.vo.llnwd.net',
+          '*.llnw.net',
+          '*.llnwd.net',
+          '*.edgefcs.net',
+          '*.akafms.net',
+          '*.edgesuite.net',
+          '*.akamaihd.net',
+          '*.analytics.edgekey.net',
+          '*.deploy.static.akamaitechnologies.com',
+          '*.cloudfront.net',
+          'hlstoken-a.akamaihd.net',
+          'vjs.zencdn.net',
+          ' *.gallerysites.net',
+          'ndla.no',
+          '*.ndla.no',
+        ],
+        frameSrc: [
+          '*.nrk.no',
+          'https://www.youtube.com',
+          'ndla.no',
+          '*.ndla.no',
+        ],
+        workerSrc: ["'self'", 'blob:'],
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "'unsafe-eval'",
+          'https://fonts.googleapis.com',
+          'https://fonts.gstatic.com',
+        ],
+        fontSrc: [
+          "'self'",
+          'https://fonts.googleapis.com',
+          'https://fonts.gstatic.com',
+          'data:',
+        ],
+        imgSrc: [
+          "'self'",
+          'https://*.ndla.no',
+          'https://www.google-analytics.com',
+          'https://stats.g.doubleclick.net',
+          'http://metrics.brightcove.com',
+          'https://httpsak-a.akamaihd.net',
+          'https://www.nrk.no/',
+          ' data:',
+        ],
+        mediaSrc: ["'self'", 'blob:', 'https://*.ndla.no'],
+        connectSrc: [
+          " 'self' ",
+          'https://*.ndla.no',
+          'https://logs-01.loggly.com',
+          'https://edge.api.brightcove.com',
+          'https://secure.brightcove.com',
+          'https://bcsecure01-a.akamaihd.net',
+          'https://hlsak-a.akamaihd.net',
+        ],
+      },
+    },
+    frameguard: process.env.NODE_ENV === 'development'
+      ? {
+        action: 'allow-from',
+        domain: '*://localhost',
+      }
+      : undefined,
+  }),
+);
 
 const renderHtmlString = (locale, userAgentString, state = {}, component = undefined) =>
   renderToString(<Html lang={locale} state={state} component={component} className={getConditionalClassnames(userAgentString)} />);
