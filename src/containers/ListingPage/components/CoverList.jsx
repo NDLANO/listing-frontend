dea8ac763aa4a30d266a07181ca7842e5cda563f
Tanja Gruschke <tmg@knowit.no>
2017-08-09 16:01:17 +0200

    #477 cleaning out inline oembed

--- src/containers/ListingPage/components/CoverList.jsx
+++ src/containers/ListingPage/components/CoverList.jsx
@@ -5,19 +5,19 @@
  *  LICENSE file in the root directory of this source tree.
  *
  */
-import React, {PropTypes, Component} from 'react';
-import {injectT} from 'ndla-i18n';
+import React, { PropTypes, Component } from 'react';
+import { injectT } from 'ndla-i18n';
 
-import {findCategoryLabel} from '../../../util/listingHelpers';
-import {CoverShape} from '../../../shapes';
+import { findCategoryLabel } from '../../../util/listingHelpers';
+import { CoverShape } from '../../../shapes';
 import ToggleOembed from "./ToggleOembed";
 
-const CoverList = ({listings, onViewOembed}) => (
+const CoverList = ({ listings, onViewOembed }) => (
   <div className="emneomrade-row">
     {listings.map(item => <CoverItem
-        key={item.id}
-        listing={item}
-        onViewOembed={onViewOembed}
+      key={item.id}
+      listing={item}
+      onViewOembed={onViewOembed}
     />)}
   </div>
 );
@@ -28,52 +28,25 @@ CoverList.propTypes = {
   onViewOembed: PropTypes.func,
 };
 
-class CoverItem extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      oembedCss: "",
-    };
-    this.onOembedButtonClick = this.onOembedButtonClick.bind(this);
-  }
-
-  onOembedButtonClick(isVisOembed) {
-    if (isVisOembed) {
-      this.setState({oembedCss: "oembed-grid"})
-    } else {
-      this.setState({oembedCss: ""})
-    }
-  }
-
-  render(){
-    const  { listing, onViewOembed } = this.props;
-
-    return(
-      <div className="produkt-container listView">
-        <div className="innerList">
-          <div className="h2-tittel-lenke" href={`/article/${listing.articleApiId}`}>
-            <div className="h2-txt-overflow">
-              {listing.title}
-            </div>
-          </div>
-          <div className="type-txt">{findCategoryLabel(listing.labels)}</div>
-          <ToggleOembed
-            onOembedButtonClick={this.onOembedButtonClick}
-            onViewOembed={onViewOembed}
-            cssClass="visfilter-btn-list"
-            url={listing.oembedUrl}/>
+const CoverItem = ({ listing, onViewOembed }) => (
+  <div className="produkt-container listView">
+    <div className="innerList">
+      <div className="h2-tittel-lenke">
+        <div className="h2-txt-overflow">
+          {listing.title}
         </div>
       </div>
-    );
-  }
-
-}
-
+      <div className="type-txt">{findCategoryLabel(listing.labels)}</div>
+      <ToggleOembed
+        onViewOembed={onViewOembed}
+        cssClass="visfilter-btn-list"
+        url={listing.oembedUrl}/>
+    </div>
+  </div>
+);
 
 CoverItem.propTypes = {
   listing: CoverShape,
-  onOembedButtonClick: PropTypes.func,
   onViewOembed: PropTypes.func,
 };
 
