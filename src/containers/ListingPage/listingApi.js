5d5f36832ce27e65648f76697f12b8b86fcc6766
Tanja Gruschke <tmg@knowit.no>
2017-08-11 10:39:46 +0200

    #477 baseOembedUrl

--- src/containers/ListingPage/listingApi.js
+++ src/containers/ListingPage/listingApi.js
@@ -9,8 +9,8 @@
 import { resolveJsonOrRejectWithError, apiResourceUrl, fetchWithAccessToken } from '../../util/apiHelpers';
 
 const baseListingUrl = apiResourceUrl('/listing-api/v1/listing');
-const baseOembedUrl ='https://test.api.ndla.no/oembed-proxy/v1/oembed';
+const baseOembedUrl = apiResourceUrl('/oembed-proxy/v1/oembed');
 
 export const fetchListingByTheme = (locale, theme) => fetchWithAccessToken(`${baseListingUrl}/theme/${theme}`).then(resolveJsonOrRejectWithError);
 
-export const fetchOembed = ( url ) => fetchWithAccessToken(`${baseOembedUrl}/?url=${encodeURI(url)}`).then(resolveJsonOrRejectWithError);
+export const fetchOembed = (url) => fetchWithAccessToken(`${baseOembedUrl}/?url=${encodeURI(url)}`).then(resolveJsonOrRejectWithError);
