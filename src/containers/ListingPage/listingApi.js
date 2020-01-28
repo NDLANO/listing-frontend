9eadaafb7e2bf99e2c846c3280f7d28955963150
Bjørnar Dalsnes <bjornar.dalsnes@knowit.no>
2020-01-28 15:57:10 +0100

    Fixed no tags bug

--- src/containers/ListingPage/listingApi.js
+++ src/containers/ListingPage/listingApi.js
@@ -12,6 +12,7 @@ const baseListingUrl = apiResourceUrl('/concept-api/v1/concepts');
 const baseTaxonomyUrl = apiResourceUrl('/taxonomy/v1/subjects/')
 
 export const fetchListing = (subjectId, pageSize) => fetch(`${baseListingUrl}?subjectIds=${subjectId}&page-size=${pageSize}`).then(resolveJsonOrRejectWithError);
-export const fetchTags = (subjectId) => fetch(`${baseListingUrl}/tags/?subjectIds=${subjectId}`).then(resolveJsonOrRejectWithError);
+export const fetchTags = (subjectId) => fetch(`${baseListingUrl}/tags/?subjectIds=${subjectId}`)
+    .then(res => res.ok ? res.json() : {});
 export const fetchSubject = (subjectId) => fetch(baseTaxonomyUrl + subjectId)
     .then(res => res.ok ? res.json() : subjectId);
