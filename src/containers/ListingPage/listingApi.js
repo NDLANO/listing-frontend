06ead3a5aa05bb31fca17154757f51750685f61d
Bjørnar Dalsnes <bjornar.dalsnes@knowit.no>
2020-02-10 14:41:31 +0100

    SubjectIds to subject in concept api

--- src/containers/ListingPage/listingApi.js
+++ src/containers/ListingPage/listingApi.js
@@ -11,8 +11,8 @@ import { resolveJsonOrRejectWithError, apiResourceUrl } from '../../util/apiHelp
 const baseListingUrl = apiResourceUrl('/concept-api/v1/concepts');
 const baseTaxonomyUrl = apiResourceUrl('/taxonomy/v1/subjects/')
 
-export const fetchListing = (subjectId, pageSize) => fetch(`${baseListingUrl}?subjectIds=${subjectId}&page-size=${pageSize}`).then(resolveJsonOrRejectWithError);
-export const fetchTags = (subjectId) => fetch(`${baseListingUrl}/tags/?subjectIds=${subjectId}`)
+export const fetchListing = (subjectId, pageSize) => fetch(`${baseListingUrl}?subjects=${subjectId}&page-size=${pageSize}`).then(resolveJsonOrRejectWithError);
+export const fetchTags = (subjectId) => fetch(`${baseListingUrl}/tags/?subjects=${subjectId}`)
     .then(res => res.ok ? res.json() : {});
 export const fetchSubject = (subjectId) => fetch(baseTaxonomyUrl + subjectId)
     .then(res => res.ok ? res.json() : subjectId);
