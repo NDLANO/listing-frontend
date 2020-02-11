2bf758bbb0bdec3309cec4ba960c4c3a11349245
Bjørnar Dalsnes <bjornar.dalsnes@knowit.no>
2020-02-11 14:02:16 +0100

    Added pop-up on selected item

--- src/containers/ListingPage/ListingPage.jsx
+++ src/containers/ListingPage/ListingPage.jsx
@@ -10,9 +10,20 @@ import PropTypes from 'prop-types';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
+import { injectT } from 'ndla-i18n';
 import ListView, { activeAlphabet } from '@ndla/listview';
 import { Select } from '@ndla/forms';
 import { OneColumn } from 'ndla-ui';
+import {
+  NotionDialogContent,
+  NotionDialogImage,
+  NotionDialogText,
+  NotionDialogTags,
+  NotionDialogLicenses,
+  NotionDialogWrapper,
+} from '@ndla/notion';
+import Modal, { ModalHeader, ModalBody, ModalCloseButton } from '@ndla/modal';
+import Button from '@ndla/button';
 
 import { mapConceptToListItem } from '../../util/listingHelpers';
 import useURIParameter from '../../util/useURIParameter';
@@ -26,7 +37,7 @@ const ListingPage = (props) => {
   const [viewStyle, setViewStyle] = useState('grid');
   const [sortByValue, setSortByValue] = useState('category');
   const [detailedItem, setDetailedItem] = useState(null);
-  const [selectItem, setSelectItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
   const [searchValue, setSearchValue] = useState('');
   const [currentSubject, setCurrentSubject] = useURIParameter('');
   const [filters, setFilters] = useQueryParameter({ subject: [], category: [] });
@@ -88,6 +99,52 @@ const ListingPage = (props) => {
   const listItems = filterItems(props.listings.listings.filter(concept => concept.subjectIds).map(concept =>
     mapConceptToListItem(concept, props.subjects.find(subject => concept.subjectIds.includes(subject.id)))));
 
+  const { t } = props;
+
+  const renderSelectedItem = () => (
+    selectedItem ? 
+    <NotionDialogWrapper
+      title={selectedItem.name}
+      subTitle={selectedItem.category.title}
+      closeCallback={setSelectedItem}>
+      <NotionDialogContent>
+        {selectedItem.image ? (
+          <NotionDialogImage
+            src={selectedItem.image}
+            alt={selectedItem.description}
+          />
+        ) : null}
+        <NotionDialogText>{selectedItem.description}</NotionDialogText>
+      </NotionDialogContent>
+      <NotionDialogTags tags={selectedItem.subject.map(subject => subject.title)}/>
+      <NotionDialogLicenses
+          license={selectedItem.license}
+          source={selectedItem.source}
+          authors={selectedItem.authors}
+          licenseBox={
+            <Modal
+              activateButton={<Button link>{t('article.useContent')}</Button>}
+              size="medium">
+              {onClose => (
+                <div>
+                  <ModalHeader modifier="no-bottom-padding">
+                    <ModalCloseButton onClick={onClose} title="lukk" />
+                  </ModalHeader>
+                  <ModalBody>
+                    <div>
+                      <h1>{t('license.heading')}</h1>
+                    </div>
+                  </ModalBody>
+                </div>
+              )}
+            </Modal>
+          }
+        />
+    </NotionDialogWrapper>
+    :
+    null
+  )
+
   return (
     <OneColumn>
       <Helmet title={'NDLA Utlisting'} />
@@ -108,11 +165,12 @@ const ListingPage = (props) => {
         alphabet={activeAlphabet(listItems)}
         detailedItem={detailedItem}
         selectCallback={setDetailedItem}
-        onSelectItem={setSelectItem}
         viewStyle={viewStyle}
         onChangedViewStyle={e => setViewStyle(e.viewStyle)}
         searchValue={searchValue}
         onChangedSearchValue={e => setSearchValue(e.target.value)}
+        selectedItem={renderSelectedItem()}
+        onSelectItem={setSelectedItem}
         filters={[
           {
             options: props.listings.filters.main.map(item => ({ title: item, value: item })),
@@ -167,4 +225,4 @@ const mapStateToProps = state => ({
   locale: getLocale(state),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ListingPage);
+export default connect(mapStateToProps, mapDispatchToProps)(injectT(ListingPage));
