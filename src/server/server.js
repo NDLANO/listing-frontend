33ce3265a1c2e65422ff6232df2129e97111fe28
Jonas Natten <jonas.natten@knowit.no>
2020-03-03 09:51:53 +0100

    Move CSP config to separate file

--- src/server/server.js
+++ src/server/server.js
@@ -10,6 +10,7 @@ import express from 'express';
 import helmet from 'helmet';
 import compression from 'compression';
 import config from '../config';
+import contentSecurityPolicy from './contentSecurityPolicy';
 
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
@@ -33,95 +34,7 @@ app.use(
       maxAge: 31536000,
       includeSubDomains: true,
     },
-    /* TODO: Rydd opp i contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'", 'blob:'],
-        scriptSrc: [
-          "'self'",
-          "'unsafe-inline'",
-          " 'unsafe-eval'",
-          'https://*.ndla.no',
-          'https://players.brightcove.net',
-          'http://players.brightcove.net',
-          'https://players.brightcove.net',
-          '*.nrk.no',
-          'https://www.googletagmanager.com',
-          'https://www.google-analytics.com',
-          'https://www.youtube.com',
-          'https://s.ytimg.com',
-          'https://cdn.auth0.com',
-          'https://vjs.zencdn.net',
-          'https://httpsak-a.akamaihd.net',
-          '*.brightcove.com',
-          '*.brightcove.net',
-          'bcove.me',
-          'bcove.video',
-          '*.api.brightcove.com',
-          '*.o.brightcove.com',
-          'players.brightcove.net',
-          'hls.ak.o.brightcove.com',
-          'uds.ak.o.brightcove.com',
-          'brightcove.vo.llnwd.net',
-          '*.llnw.net',
-          '*.llnwd.net',
-          '*.edgefcs.net',
-          '*.akafms.net',
-          '*.edgesuite.net',
-          '*.akamaihd.net',
-          '*.analytics.edgekey.net',
-          '*.deploy.static.akamaitechnologies.com',
-          '*.cloudfront.net',
-          'hlstoken-a.akamaihd.net',
-          'vjs.zencdn.net',
-          ' *.gallerysites.net',
-          'ndla.no',
-          '*.ndla.no',
-          'localhost:3001',
-        ],
-        frameSrc: [
-          '*.nrk.no',
-          'https://www.youtube.com',
-          'ndla.no',
-          '*.ndla.no',
-        ],
-        workerSrc: ["'self'", 'blob:'],
-        styleSrc: [
-          "'self'",
-          "'unsafe-inline'",
-          "'unsafe-eval'",
-          'https://fonts.googleapis.com',
-          'https://fonts.gstatic.com',
-        ],
-        fontSrc: [
-          "'self'",
-          'https://fonts.googleapis.com',
-          'https://fonts.gstatic.com',
-          'data:',
-        ],
-        imgSrc: [
-          "'self'",
-          'https://*.ndla.no',
-          'https://www.google-analytics.com',
-          'https://stats.g.doubleclick.net',
-          'http://metrics.brightcove.com',
-          'https://httpsak-a.akamaihd.net',
-          'https://www.nrk.no/',
-          ' data:',
-        ],
-        mediaSrc: ["'self'", 'blob:', 'https://*.ndla.no'],
-        connectSrc: [
-          " 'self' ",
-          'https://*.ndla.no',
-          'https://logs-01.loggly.com',
-          'https://edge.api.brightcove.com',
-          'https://secure.brightcove.com',
-          'https://bcsecure01-a.akamaihd.net',
-          'https://hlsak-a.akamaihd.net',
-          'http://localhost:3001',
-          'ws://localhost:3001',
-        ],
-      },
-    }, */
+    contentSecurityPolicy,
     frameguard:
       process.env.NODE_ENV === 'development'
         ? {
