1c40c62fe5407aea1606f3915c2107bb14f1140c
N4G1 <bartezzzzz@hotmail.com>
2020-11-26 09:34:36 +0100

    turned App.jsx to App.tsx

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -65,7 +65,6 @@ export class App extends React.Component {
                   conceptId={Number(routeProps.match.params.conceptId)}
                   inModal={false}
                   language={routeProps.match.params.selectedLanguage || locale}
-                  locale={locale}
                 />
               )}
             />
