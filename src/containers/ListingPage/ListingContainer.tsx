faed766e6233c713e575301259a6e53e3674d903
sigrunnu <sigrun.nummedal@gmail.com>
2021-06-30 13:57:19 +0200

    WIP revert changes

--- src/containers/ListingPage/ListingContainer.tsx
+++ src/containers/ListingPage/ListingContainer.tsx
@@ -5,159 +5,160 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-import React, { useState, useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-
-import ListingView from './ListingView';
-// @ts-ignore
-import useQueryParameter from '../../util/useQueryParameter';
-// @ts-ignore
-import { getTagsParameter } from '../../util/listingHelpers';
-import { conceptSearchQuery } from '../../queries';
-import {
-  Location,
-  Subject,
-  Filter,
-  ListItem,
-  ConceptSearch,
-} from '../../interfaces';
-
-const PAGE_SIZE = 100;
-
-interface Props {
-  isOembed: boolean;
-  subjects: Subject[];
-  tags: string[];
-  filters: Map<string, Filter>;
-  location: Location;
-  locale: string;
-}
-
-const ListingContainer = ({
-  isOembed,
-  subjects,
-  tags,
-  filters,
-  location,
-  locale,
-}: Props): JSX.Element => {
-  const [filterListOpen, setFilterListOpen] = useState(false);
-  const [searchValue, setSearchValue] = useState('');
-  const [queryParams, setQueryParams] = useQueryParameter({
-    subjects: [],
-    filters: [],
-    concept: undefined,
-  });
-
-  const useDebounce = (val: string, delay: number): string => {
-    const [debouncedVal, setDebouncedVal] = useState(val);
-    useEffect(() => {
-      const handler = setTimeout(() => {
-        setDebouncedVal(val);
-      }, delay);
-      return (): void => clearTimeout(handler);
-    }, [val, delay]);
-    return `${debouncedVal}*`;
-  };
-  const debouncedSearchVal = useDebounce(searchValue, 200);
-
-  const { data, loading, fetchMore } = useQuery<ConceptSearch>(
-    conceptSearchQuery,
-    {
-      variables: {
-        query: debouncedSearchVal,
-        subjects: queryParams.subjects.join(),
-        tags: getTagsParameter(tags, queryParams.filters),
-        pageSize: PAGE_SIZE.toString(),
-        language: locale,
-        fallback: true,
-        exactMatch: false,
-      },
-      notifyOnNetworkStatusChange: true, // For spinner on load more
-    },
-  );
-
-  const handleSelectItem = (value: ListItem): void => {
-    setQueryParams({
-      ...queryParams,
-      concept: value?.id,
-    });
-  };
-
-  const handleChangeSubject = (values: string[]): void => {
-    setQueryParams({
-      subjects: values,
-      filters: [],
-    });
-  };
-
-  const handleChangeListFilter = (value: string | null): void => {
-    setFilterListOpen(false);
-    setQueryParams({
-      subjects: [],
-      filters: [value],
-    });
-  };
-
-  const handleRemoveFilter = (): void => {
-    setQueryParams({
-      ...queryParams,
-      filters: [],
-    });
-  };
-
-  const handleChangeFilters = (_: string, values: string[]): void => {
-    setQueryParams({
-      ...queryParams,
-      filters: values,
-    });
-  };
-
-  const onLoadMoreClick = (): void => {
-    if (data) {
-      fetchMore({
-        variables: {
-          page: `${Math.floor(
-            data.conceptSearch.concepts.length / PAGE_SIZE + 1,
-          )}`,
-        },
-      });
-    }
-  };
-
-  const totalCount = data?.conceptSearch?.totalCount;
-  const concepts = data?.conceptSearch?.concepts;
-  const showLoadMore =
-    concepts !== undefined && totalCount !== undefined
-      ? concepts.length < totalCount
-      : true;
-
-  return (
-    <ListingView
-      isOembed={isOembed}
-      loading={loading}
-      showLoadMore={!!showLoadMore}
-      filterListOpen={filterListOpen}
-      setFilterListOpen={setFilterListOpen}
-      totalCount={totalCount || 0}
-      concepts={concepts || []}
-      subjects={subjects}
-      filters={filters}
-      selectedSubjects={queryParams.subjects}
-      selectedFilters={queryParams.filters}
-      selectedConcept={queryParams.concept}
-      selectedListFilter={queryParams.filters?.[0]}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      onLoadMoreClick={onLoadMoreClick}
-      handleSelectItem={handleSelectItem}
-      handleChangeListFilter={handleChangeListFilter}
-      handleRemoveFilter={handleRemoveFilter}
-      handleChangeSubject={handleChangeSubject}
-      handleChangeFilters={handleChangeFilters}
-      location={location}
-    />
-  );
-};
-
-export default ListingContainer;
+ import React, { useState, useEffect } from 'react';
+ import { useQuery } from '@apollo/client';
+ 
+ import ListingView from './ListingView';
+ // @ts-ignore
+ import useQueryParameter from '../../util/useQueryParameter';
+ // @ts-ignore
+ import { getTagsParameter } from '../../util/listingHelpers';
+ import { conceptSearchQuery } from '../../queries';
+ import {
+   Location,
+   Subject,
+   Filter,
+   ListItem,
+   ConceptSearch,
+ } from '../../interfaces';
+ 
+ const PAGE_SIZE = 100;
+ 
+ interface Props {
+   isOembed: boolean;
+   subjects: Subject[];
+   tags: string[];
+   filters: Map<string, Filter>;
+   location: Location;
+   locale: string;
+ }
+ 
+ const ListingContainer = ({
+   isOembed,
+   subjects,
+   tags,
+   filters,
+   location,
+   locale,
+ }: Props): JSX.Element => {
+   const [filterListOpen, setFilterListOpen] = useState(false);
+   const [searchValue, setSearchValue] = useState('');
+   const [queryParams, setQueryParams] = useQueryParameter({
+     subjects: [],
+     filters: [],
+     concept: undefined,
+   });
+ 
+   const useDebounce = (val: string, delay: number): string => {
+     const [debouncedVal, setDebouncedVal] = useState(val);
+     useEffect(() => {
+       const handler = setTimeout(() => {
+         setDebouncedVal(val);
+       }, delay);
+       return (): void => clearTimeout(handler);
+     }, [val, delay]);
+     return `${debouncedVal}*`;
+   };
+   const debouncedSearchVal = useDebounce(searchValue, 200);
+ 
+   const { data, loading, fetchMore } = useQuery<ConceptSearch>(
+     conceptSearchQuery,
+     {
+       variables: {
+         query: debouncedSearchVal,
+         subjects: queryParams.subjects.join(),
+         tags: getTagsParameter(tags, queryParams.filters),
+         pageSize: PAGE_SIZE.toString(),
+         language: locale,
+         fallback: true,
+         exactMatch: false,
+       },
+       notifyOnNetworkStatusChange: true, // For spinner on load more
+     },
+   );
+ 
+   const handleSelectItem = (value: ListItem): void => {
+     setQueryParams({
+       ...queryParams,
+       concept: value?.id,
+     });
+   };
+ 
+   const handleChangeSubject = (values: string[]): void => {
+     setQueryParams({
+       subjects: values,
+       filters: [],
+     });
+   };
+ 
+   const handleChangeListFilter = (value: string | null): void => {
+     setFilterListOpen(false);
+     setQueryParams({
+       subjects: [],
+       filters: [value],
+     });
+   };
+ 
+   const handleRemoveFilter = (): void => {
+     setQueryParams({
+       ...queryParams,
+       filters: [],
+     });
+   };
+ 
+   const handleChangeFilters = (_: string, values: string[]): void => {
+     setQueryParams({
+       ...queryParams,
+       filters: values,
+     });
+   };
+ 
+   const onLoadMoreClick = (): void => {
+     if (data) {
+       fetchMore({
+         variables: {
+           page: `${Math.floor(
+             data.conceptSearch.concepts.length / PAGE_SIZE + 1,
+           )}`,
+         },
+       });
+     }
+   };
+ 
+   const totalCount = data?.conceptSearch?.totalCount;
+   const concepts = data?.conceptSearch?.concepts;
+   const showLoadMore =
+     concepts !== undefined && totalCount !== undefined
+       ? concepts.length < totalCount
+       : true;
+ 
+   return (
+     <ListingView
+       isOembed={isOembed}
+       loading={loading}
+       showLoadMore={!!showLoadMore}
+       filterListOpen={filterListOpen}
+       setFilterListOpen={setFilterListOpen}
+       totalCount={totalCount || 0}
+       concepts={concepts || []}
+       subjects={subjects}
+       filters={filters}
+       selectedSubjects={queryParams.subjects}
+       selectedFilters={queryParams.filters}
+       selectedConcept={queryParams.concept}
+       selectedListFilter={queryParams.filters?.[0]}
+       searchValue={searchValue}
+       setSearchValue={setSearchValue}
+       onLoadMoreClick={onLoadMoreClick}
+       handleSelectItem={handleSelectItem}
+       handleChangeListFilter={handleChangeListFilter}
+       handleRemoveFilter={handleRemoveFilter}
+       handleChangeSubject={handleChangeSubject}
+       handleChangeFilters={handleChangeFilters}
+       location={location}
+       locale={locale}
+     />
+   );
+ };
+ 
+ export default ListingContainer;
--- src/containers/ListingPage/ListingPage.tsx
+++ src/containers/ListingPage/ListingPage.tsx
@@ -5,44 +5,45 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-import React from 'react';
-import { useQuery } from '@apollo/client';
-// @ts-ignore
-import { Spinner } from '@ndla/ui';
-
-// @ts-ignore
-import { mapTagsToFilters, filterTags } from '../../util/listingHelpers';
-import ListingContainer from './ListingContainer';
-// @ts-ignore
-import NotFoundPage from '../NotFoundPage/NotFoundPage';
-import { listingPageQuery } from '../../queries';
-import { Location, ListingPageType } from '../../interfaces';
-
-interface Props {
-  isOembed: boolean;
-  locale: string;
-  location: Location;
-}
-
-const ListingPage = ({ locale, location, isOembed }: Props): JSX.Element => {
-  const { data, loading } = useQuery<ListingPageType>(listingPageQuery);
-
-  if (loading) return <Spinner />;
-  if (!data) return <NotFoundPage />;
-
-  const filters = mapTagsToFilters(data.listingPage.tags);
-  const filteredTags = filterTags(data.listingPage.tags);
-
-  return (
-    <ListingContainer
-      isOembed={isOembed}
-      subjects={data.listingPage.subjects}
-      tags={filteredTags}
-      filters={filters}
-      location={location}
-      locale={locale}
-    />
-  );
-};
-
-export default ListingPage;
+ import React from 'react';
+ import { useQuery } from '@apollo/client';
+ // @ts-ignore
+ import { Spinner } from '@ndla/ui';
+ 
+ // @ts-ignore
+ import { mapTagsToFilters, filterTags } from '../../util/listingHelpers';
+ import ListingContainer from './ListingContainer';
+ // @ts-ignore
+ import NotFoundPage from '../NotFoundPage/NotFoundPage';
+ import { listingPageQuery } from '../../queries';
+ import { Location, ListingPageType } from '../../interfaces';
+ 
+ interface Props {
+   isOembed: boolean;
+   locale: string;
+   location: Location;
+ }
+ 
+ const ListingPage = ({ locale, location, isOembed }: Props): JSX.Element => {
+   const { data, loading } = useQuery<ListingPageType>(listingPageQuery);
+ 
+   if (loading) return <Spinner />;
+   if (!data) return <NotFoundPage />;
+ 
+   const filters = mapTagsToFilters(data.listingPage.tags);
+   const filteredTags = filterTags(data.listingPage.tags);
+ 
+   return (
+     <ListingContainer
+       isOembed={isOembed}
+       subjects={data.listingPage.subjects}
+       tags={filteredTags}
+       filters={filters}
+       location={location}
+       locale={locale}
+     />
+   );
+ };
+ 
+ export default ListingPage;
+ 
