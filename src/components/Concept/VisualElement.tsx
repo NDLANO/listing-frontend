d7ee3c6a532c177dd370dbd4977ebce885402af8
Gunnar Velle <gunnar.velle@gmail.com>
2021-12-15 14:35:54 +0100

    format.

--- src/components/Concept/VisualElement.tsx
+++ src/components/Concept/VisualElement.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import {
-  colors,
-  spacing,
-  fonts,
-} from '@ndla/core';
+import { colors, spacing, fonts } from '@ndla/core';
 import Image from '@ndla/ui/lib/Image';
 import { getLicenseByAbbreviation, LicenseByline } from '@ndla/licenses';
 import { GQLVisualElement, GQLImageElement } from '../../graphqlTypes';
