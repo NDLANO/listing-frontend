93b7024c35f93977ed26511cab4f2dfd85df1f59
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-01-20 09:20:44 +0100

    Removed a couple of unused dependencies

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const HTTP_OK = 200;
+export const HTTP_INTERNAL_SERVER_ERROR = 500;
+export const HTTP_BAD_REQUEST = 400;
+
+export const httpStatus = {
+  ok: 200,
+  internalServerError: 500,
+  badRequest: 400,
+  movedPermanently: 301,
+  temporaryRedirect: 307,
+};
