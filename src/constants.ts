3efbdcb9e430643251a1b67bab99ccb2e54b16b5
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-01-20 09:38:24 +0100

    Remove unused http consts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
-export const HTTP_OK = 200;
-export const HTTP_INTERNAL_SERVER_ERROR = 500;
-export const HTTP_BAD_REQUEST = 400;
-
 export const httpStatus = {
   ok: 200,
   internalServerError: 500,
